023d950ec5944bdc2b2396fdd0eb3895ca03e24f
Mark Edney <68298107+mark-edney@users.noreply.github.com>
2024-08-09 14:52:30 -0400

    Update data_Validation.js
    Fixed an issue were I was using the wrong variable.
    Added data validation for the instrumentation table with the UOM lookup

--- scripts/data_Validation.js
+++ scripts/data_Validation.js
@@ -59,7 +59,7 @@ async function data_Validation() {
     //Applying validation for closed lists
     for(let i = 0; i< affected.length; i++){
       let affectedRange = assetTable.columns.getItem(affected[i]).getDataBodyRange().load();
-      let validationRange = sheet.tables.getItem(validation[i]).load('name');
+      let affectedValidation = sheet.tables.getItem(validation[i]).load('name');
       await context.sync();
 
       //Clearing and applying new rule
@@ -67,7 +67,7 @@ async function data_Validation() {
       affectedRange.dataValidation.rule = {
         list: {
           inCellDropDown: true,
-          source: "=INDIRECT(\"" + validationRange.name + "\")"
+          source: "=INDIRECT(\"" + affectedValidation.name + "\")"
         }
       };
       affectedRange.dataValidation.errorAlert = {
@@ -82,18 +82,18 @@ async function data_Validation() {
     affected = [];
     validation = [];
     
-    //Filters for open lists and on the asst table
+    //Filters for open lists and on the asset table
     for (let i = 0; i < tableObjectName.values.length; i++) {
       if (tableObjectName.values[i][0] == "asset_Table" && validationType.values[i][0] == "open list validation") {
         affected.push(tableColumns.values[i][0]);
         validation.push(validationRange.values[i][0]);
         }
       }
-
+    
     //Applying validation for open lists
     for (let i = 0; i < affected.length; i++) {
       let affectedRange = assetTable.columns.getItem(affected[i]).getDataBodyRange().load();
-      let validationRange = sheet.tables.getItem(validation[i]).load('name');
+      let affectedValidation = sheet.tables.getItem(validation[i]).load('name');
       await context.sync();
 
       //Clearing and applying new rule
@@ -101,7 +101,7 @@ async function data_Validation() {
       affectedRange.dataValidation.rule = {
         list: {
           inCellDropDown: true,
-          source: "=INDIRECT(\"" + validationRange.name + "\")"
+          source: "=INDIRECT(\"" + affectedValidation.name + "\")"
           }
           };
       affectedRange.dataValidation.errorAlert = {
@@ -115,32 +115,72 @@ async function data_Validation() {
       let formatting = affectedRange.conditionalFormats.add(Excel.ConditionalFormatType.custom);
       let cellAddress = getColLetter(affectedRange.columnIndex + 1);
 
-      formatting.custom.rule.formula = "=AND(ISERROR(MATCH(" + cellAddress + "5, INDIRECT(\"" + validationRange.name + "\"), 0)), NOT(ISBLANK(" + cellAddress + "5)))";
+      formatting.custom.rule.formula = "=AND(ISERROR(MATCH(" + cellAddress + "5, INDIRECT(\"" + affectedValidation.name + "\"), 0)), NOT(ISBLANK(" + cellAddress + "5)))";
       formatting.custom.format.fill.color ="yellow";
+
+      //Variables to hold filtered values
+      affected = [];
+      validation = [];
+
+      let instrumentationTable = sheet.tables.getItem("instrument_Table").load("values");
+      await context.sync();
+
+      //Filters for open lists and on the instrumentaiton table
+      for (let i = 0; i < tableObjectName.values.length; i++) {
+        if (tableObjectName.values[i][0] == "instrument_Table" && validationType.values[i][0] == "open list validation") {
+          affected.push(tableColumns.values[i][0]);
+          validation.push(validationRange.values[i][0]);
+        }
+      }
+
+      //Applying validation for open lists
+      for (let i = 0; i < affected.length; i++) {
+        let affectedRange = instrumentationTable.columns.getItem(affected[i]).getDataBodyRange().load();
+        await context.sync();
+
+        //Clearing and applying new rule
+        affectedRange.dataValidation.clear();
+        affectedRange.dataValidation.rule = {
+          list: {
+            inCellDropDown: true,
+            source: "=INDIRECT(\"" + validation[i] + "\")"
+          }
+        };
+        affectedRange.dataValidation.errorAlert = {
+          message: "Not valididated value",
+          showAlert: true, // The default is 'true'.
+          style: Excel.DataValidationAlertStyle.warning,
+          title: "Unvalidated Value"
+        };
+
+        affectedRange.conditionalFormats.clearAll();
+        let formatting = affectedRange.conditionalFormats.add(Excel.ConditionalFormatType.custom);
+        let cellAddress = getColLetter(affectedRange.columnIndex + 1);
+
+        formatting.custom.rule.formula = "=AND(ISERROR(MATCH($C14, INDIRECT(\"" + validation[i] + "\"), 0)), NOT(ISBLANK($C14)))";
+        formatting.custom.format.fill.color = "yellow";
+
+        //Instrumentation Parameter UOM, seperate code
+        let affectedRange = instrumentationTable.columns.getItem("Parameter UOM (open value list)").getDataBodyRange().load();
+        await context.sync();
+
+        //Clearing and applying new rule
+        affectedRange.dataValidation.clear();
+        affectedRange.dataValidation.rule = {
+          list: {
+            inCellDropDown: true,
+            source: "=OFFSET(XLOOKUP($C14,INDIRECT(\"paramter_Type[[#All], [Parameter Type]]\"), INDIRECT(\"paramter_Type[[#All], [UOM1]]\")),0,0,1,COUNTA(XLOOKUP($C14, INDIRECT(\"paramter_Type[[#All], [Parameter Type]]\"),INDIRECT(\"paramter_Type[#All]\")))-1)"
+          }
+        };
+        affectedRange.dataValidation.errorAlert = {
+          message: "Not valididated value",
+          showAlert: true, // The default is 'true'.
+          style: Excel.DataValidationAlertStyle.warning,
+          title: "Unvalidated Value"
+        };
+
+      }
     }
   await context.sync();
   });
 }
-
-// Function to get the column letter from the column number
-function getColLetter(columnNumber: number): string {
-  let temp: number = columnNumber;
-  let letter: string = '';
-  while (temp > 0) {
-    let mod: number = (temp - 1) % 26;
-    letter = String.fromCharCode(65 + mod) + letter;
-    temp = Math.floor((temp - mod) / 26);
-  }
-  return letter;
-}
-
-/** Default helper for invoking an action and handling errors. */
-async function tryCatch(callback) {
-  try {
-    await callback();
-  } catch (error) {
-    // Note: In a production add-in, you'd want to notify the user through your add-in's UI.
-    console.error(error);
-  }
-}
-
